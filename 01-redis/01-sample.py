adad95fbfe6c94d04b41ee479cd09b4898f15615
wanggang01 <wg13530159875@163.com>
2019-08-18 11:12:24 +0800

    这个是我的第二次

--- 01-redis/01-sample.py
+++ 01-redis/01-sample.py
@@ -5,3 +5,13 @@ r = redis.Redis(password="123456", decode_responses=True)
 r.set("xxx","yyy")
 
 print(r.get("xxx"))
+
+
+
+
+
+
+
+
+
+
--- 01-redis/02-pool.py
+++ 01-redis/02-pool.py
@@ -7,3 +7,15 @@ pool = redis.ConnectionPool(password="123456", decode_responses=True)
 r = redis.Redis(connection_pool=pool)
 
 print(r.get("xxx"))
+
+
+
+
+#用python连接redis数据库
+
+import redis
+#创建连接池
+pool = redis.ConnectionPool(password='123456', decode_responses=True)
+
+#使用连接池对象去链接redis
+r = redis.Redis(connection_pool=pool)
